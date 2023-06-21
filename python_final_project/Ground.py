88971f52f4951f673fa2049babcc16873597e878
SpaceJ0392 <125726586+SpaceJ0392@users.noreply.github.com>
2023-06-21 12:25:14 +0900

    Update Ground.py

--- python_final_project/Ground.py
+++ python_final_project/Ground.py
@@ -9,8 +9,5 @@ class Ground(Sprite):
         # 플레이어가 위치할 배경
         self.goto(x, y)
         self.stamp()
-
-        # self.goto(800, -100)
-        # self.stamp()
-
+        
         return self
